6a40156ef45b0b025166fefaa75cf131a0f6d224
Cristhianjo <cristhianbrice16@gmail.com>
2017-07-12 20:25:08 -0500

    Creacion de los metodos suma flotante y resta flotante y su documentacion

--- examenConjunto/src/main/java/ec/edu/espe/educat/examenconjunto/Operaciones.java
+++ examenConjunto/src/main/java/ec/edu/espe/educat/examenconjunto/Operaciones.java
@@ -5,49 +5,96 @@
  */
 package ec.edu.espe.educat.examenconjunto;
 
-/**Clase Operaciones
+/**
+ * Clase Operaciones
+ *
  * @author CRIS
  * @version 12/07/2017
  */
-
 public class Operaciones {
-    
- /**Creación del método multiplicacion
- * @param n1 primero numero a multiplicar
- * @param n2 segundo numero a multiplicar
- */
-    
-    int multiplicacion(int n1,int n2) {
+
+    /**
+     * Creación del método suma.
+     *
+     * @param numero1 primero numero a suma
+     * @param numero2 segundo numero a suma
+     * @author Cristhian Arevalo
+     */
+    public int suma(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+
+    /**
+     * Creación del método resta.
+     *
+     * @param numero1 primero numero a resta.
+     * @param numero2 segundo numero a resta.
+     * @author Cristhian Arevalo
+     */
+    public int resta(int numero1, int numero2) {
+        return numero1 - numero2;
+    }
+
+    /**
+     * Creacion de metodo suma flotante.
+     *
+     * @param numero1 primero numero a suma
+     * @param numero2 segundo numero a suma
+     * @author Cristhian Arevalo
+     */
+    public float suma(float numero1, float numero2) {
+        return numero1 + numero2;
+    }
+
+    /**
+     * Creacion de metodo resta flotante.
+     *
+     * @param numero1 primero numero a resta.
+     * @param numero2 segundo numero a resta.
+     * @author Cristhian Arevalo
+     */
+    public float resta(float numero1, float numero2) {
+        return numero1 - numero2;
+    }
+
+    /**
+     * Creación del método multiplicacion
+     *
+     * @param n1 primero numero a multiplicar
+     * @param n2 segundo numero a multiplicar
+     */
+    int multiplicacion(int n1, int n2) {
         return n1 * n2;
     }
 
- /**Creación del método division
- * @param n1 primero numero a dividir
- * @param n2 segundo numero a dividir
- */
+    /**
+     * Creación del método division
+     *
+     * @param n1 primero numero a dividir
+     * @param n2 segundo numero a dividir
+     */
     int division(int n1, int n2) {
         return n1 / n2;
     }
-    
-    
-    /**Creación del método multiplicacionf
- * @param n1 primero numero a multiplicar
- * @param n2 segundo numero a multiplicar
- */
-    
-    float multiplicacionf(float n1,float n2) {
+
+    /**
+     * Creación del método multiplicacionf
+     *
+     * @param n1 primero numero a multiplicar
+     * @param n2 segundo numero a multiplicar
+     */
+    float multiplicacionf(float n1, float n2) {
         return n1 * n2;
     }
 
- /**Creación del método divisionf
- * @param n1 primero numero a dividir
- * @param n2 segundo numero a dividir
- */
-   float divisionf(float n1, float n2) {
+    /**
+     * Creación del método divisionf
+     *
+     * @param n1 primero numero a dividir
+     * @param n2 segundo numero a dividir
+     */
+    float divisionf(float n1, float n2) {
         return n1 / n2;
     }
-    
-    
-    
-    
+
 }
