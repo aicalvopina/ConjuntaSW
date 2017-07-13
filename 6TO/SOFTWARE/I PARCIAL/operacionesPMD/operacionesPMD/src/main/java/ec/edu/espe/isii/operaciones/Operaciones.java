aded878b953f197a2b4bcf8a2ab37b27583c4318
CRIS <CRIS@10.9.4.74>
2017-07-12 20:01:52 -0500

    creacion metodos multiplicacion y division

---
+++ 6TO/SOFTWARE/I PARCIAL/operacionesPMD/operacionesPMD/src/main/java/ec/edu/espe/isii/operaciones/Operaciones.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.isii.operaciones;
+
+/**
+ *
+ * @author Labs-DECC
+ */
+public class Operaciones {
+    
+    int suma(final int numero1, final int numero2) {
+        return numero1 + numero2;
+    }
+
+    int resta(final int numero1, final int numero2) {
+        return numero1 - numero2;
+    }
+
+    int multi(final int numero1, final int numero2) {
+        return numero1 * numero2;
+    }
+
+    float division(final float numero1, final float numero2) {
+        return numero1 / numero2;
+    }
+}
