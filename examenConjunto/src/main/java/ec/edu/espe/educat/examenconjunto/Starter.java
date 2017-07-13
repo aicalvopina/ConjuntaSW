ae7a34e442601277f6b315ac680155250dba7ff8
adrian <adrian>
2017-07-12 20:00:40 -0500

    Creacion de la clase Starter

---
+++ examenConjunto/src/main/java/ec/edu/espe/educat/examenconjunto/Starter.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.educat.examenconjunto;
+
+/**
+ *  Clase principal que servira de menu para el proyecto.
+ * @author adrian
+ */
+public class Starter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*
+        *Objeto de la clase operaciones para 
+        */
+        Operaciones operaciones = new Operaciones();
+        
+    }
+    
+}
